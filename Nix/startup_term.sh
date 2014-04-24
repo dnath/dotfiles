7759f9f3d20aa8c6ea296326851ea149b36e30b8
Dibyendu Nath <dev.nath.cs@gmail.com>
2014-04-23 19:36:17 -0700

    Added zshrc appending + handle params

--- Nix/startup_term.sh
+++ Nix/startup_term.sh
@@ -1,19 +1,31 @@
 #! /bin/sh
 
-## force re-download and setup
-if [ $1 = '-f' ]; then
-  rm -f ~/.vim/autoload/pathogen.vim
-  rm -rf ~/.vim/bundle/nerdtree
-  rm -f ~/.vimrc
-  rm -rf ~/.oh-my-zsh
-fi
-
-## help
-if [ $1 = '-h' ]; then
-  echo 'startup_term.sh [-f] [-h]'
-  echo '-f : force re-download and setup'
-  echo '-h : help'
-fi
+## handle parameters
+handle_params( ) {
+  for param in $*
+  do
+    ## force re-download and setup
+    if [ $param = '-f' ]; then
+      rm -f ~/.vim/autoload/pathogen.vim
+      rm -rf ~/.vim/bundle/nerdtree
+      rm -f ~/.vimrc
+      rm -rf ~/.oh-my-zsh
+    fi
+    
+    ## help
+    if [ $param = '-h' ]; then
+      echo 'startup_term.sh [-f] [-h]'
+      echo '-f : force re-download and setup'
+      echo '-h : help'
+      exit
+    fi
+    
+    ## append zshrc
+    if [ $param = '-z' ]; then
+      append_zshrc=true
+    fi
+  done
+}
 
 ## check time stamps of two files
 check_time_stamp( ) {
@@ -31,6 +43,9 @@ check_time_stamp( ) {
     return $2
   fi
 }
+
+append_zshrc=false
+handle_params $*
 
 ### vim
 ##  pathogen
@@ -66,10 +81,22 @@ if [ ! -d ~/.oh-my-zsh ]; then
   curl -L "https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
 fi
 
+## oh-my-zsh theme
 if [ ! -f ~/.oh-my-zsh/themes/agnoster_mod.zsh-theme ]; then
-  echo 'Copying my modified version of agnoster'
+  echo 'Copying my modified version of agnoster...'
   curl -Sso ~/.oh-my-zsh/themes/agnoster_mod.zsh-theme \
     https://raw.github.com/dnath/config/master/Nix/agnoster_mod.zsh-theme
-  sed -i '/ZSH_THEME="robbyrussel"/c\ZSH_THEME="agnoster_mod"' ~/.zshrc
+  if [ -f ~/.zshrc ]; then
+    sed -i '/ZSH_THEME="robbyrussell"/c\ZSH_THEME="agnoster_mod"' ~/.zshrc
+  else
+    echo 'Unable to set zsh theme as .vimrc is missing !'
+  fi
+fi
+
+## add to zshrc
+if [ -f ~/.zshrc  && append_zshrc = true ]; then
+  curl -Sso zshrc_tmp https://raw.github.com/dnath/config/master/Nix/.zshrc
+  cat zshrc_tmp >> ~/.zshrc
+  rm -f zshrc_tmp
 fi
 
