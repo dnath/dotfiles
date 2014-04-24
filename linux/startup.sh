cdf06090f0aa1d5e04a4690c8be292c8c4258c02
Dibyendu Nath <dev.nath.cs@gmail.com>
2014-04-23 18:20:16 -0700

    Added python install

--- linux/startup.sh
+++ linux/startup.sh
@@ -11,31 +11,43 @@ fi
 
 echo 'Repo Update command = ' $cmd
 
+## curl
 if [ -z "$(command -v curl)" ]; then
   echo 'Installing curl...'
   sudo $cmd -y install curl
 fi
 
-if [ -z "$(command -v vim)" ]; then
-  echo 'Installing vim...'
-  sudo $cmd -y install vim
-fi
-
-if [ -z "$(command -v git)" ]; then
-  echo 'Installing git...'
-  sudo $cmd -y install git
-fi
-
-if [ -z "$(command -v gcc)" ]; then
-  echo 'Installing gcc...'
-  sudo $cmd -y install gcc
-fi
-
+## build-essential : make
 if [ -z "$(command -v make)" ]; then
   echo 'Installing build-essential...'
   sudo $cmd -y install build-essential
 fi
 
+## gcc
+if [ -z "$(command -v gcc)" ]; then
+  echo 'Installing gcc...'
+  sudo $cmd -y install gcc
+fi
+
+## git
+if [ -z "$(command -v git)" ]; then
+  echo 'Installing git...'
+  sudo $cmd -y install git
+fi
+
+## vim
+if [ -z "$(command -v vim)" ]; then
+  echo 'Installing vim...'
+  sudo $cmd -y install vim
+fi
+
+## python
+if [ -z "$(command -v python)" ]; then
+  echo 'Installing python...'
+  sudo $cmd -y install python
+fi
+
+## javac
 #if [ -z "$(command -v javac)" ]; then
 #  echo 'Installing openjdk-7-jdk icedtea-7-plugin...'
 #  sudo $cmd -y install openjdk-7-jdk icedtea-7-plugin
@@ -49,5 +61,5 @@ echo 'Setting zsh as default shell... [Relogin to get zsh]'
 sudo chsh -s "$(which zsh)"
 
 ## calling startup_term.sh 
+echo 'Calling startup_term.sh ...'
 curl -L https://raw.github.com/dnath/config/master/Nix/startup_term.sh | sh
-
